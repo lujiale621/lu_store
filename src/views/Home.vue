871d466f0d7f5d767993612ca3c60dcd0f5e1a37
lujiale621 <751874332@qq.com>
2021-04-09 17:09:52 +0800

    s

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,27 @@
 <template>
   <div>
     <div class="title">
-    
+      <van-nav-bar left-arrow :border="false">
+        <template #title>
+          <van-search shape="round" v-model="value" placeholder="请输入搜索关键词" @search="search"></van-search>
+        </template>
+        <template #left>
+          <img class="icon-avatar" src="../assets/av.png" alt="Smiley face" width="30" height="30" />
+        </template>
+        <template #right>
+          <van-icon :size="25" class="iconfont icon-email-fill" name="sixin" />
+          <van-icon :size="25" class="iconfont icon-clock-fill" name="lishijilu" />
+        </template>
+      </van-nav-bar>
     </div>
     <div class="body"></div>
     <div class="footer"></div>
   </div>
 </template>
 <script>
-
 export default {
   name: "home",
-   components: {  }
+  components: {},
 };
 </script>
 <style scoped>
